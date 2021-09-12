dc2774d195401d59294fa024d96c337c9d43af92
Calapzu <yamsoncalapzu@gmail.com>
2021-09-12 18:32:11 -0500

    Se agrega el metodo HTTP DELETE borrarPorId

--- src/main/java/com/sofkau/crudPerson/controlador/ControladorPersona.java
+++ src/main/java/com/sofkau/crudPerson/controlador/ControladorPersona.java
@@ -24,11 +24,18 @@ public class ControladorPersona {
     public Optional<Persona> listaPersonasPorId(@PathVariable("id") int id) {
         return servicio.listarId(id);
     }
-    
+
     @PostMapping(value = "/guardarPersonas")
     public Persona guardarPersona(@RequestBody Persona persona) {
         return servicio.guardar(persona);
     }
+
+    @DeleteMapping(value = "/eliminarPersonaId/{id}")
+    public void borrarPorId(@PathVariable("id") int id) {
+        servicio.borrar(id);
+
+    }
+
 
 
 
